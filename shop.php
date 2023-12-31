1732e82ea24581cbb7c5021233d588f3e92c00e4
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-01-01 04:41:57 +0500

    peredelavanie_magazina_and_popitka_nachala_mobile_adaptation

--- shop.php
+++ shop.php
@@ -73,143 +73,26 @@ if ((isset($_COOKIE['izm']))&&(isset($_COOKIE['izm1']))&&(isset($_COOKIE['izm2']
 
 </header>
 
-<main id='search-container'>
+<main>
+	<div class="container">
+		<div class="product">
+			<section class="form_poisk">
+				<input type='text' class='poisk' placeholder="Поиск">
+			</section>
+			<div class="pet">
+				<section class="dog">
 
-<!-- <input type="text" name="poisk" class="form-style" placeholder="Поиск...." id="search-bar" autocomplete="off"> -->
+				</section>
+				<section class='cat'>
 
-<!-- <div id="results-container"></div>
-<p id="movie-unavailable-txt"></p> -->
-	<div class='wraper'>
-		<h1 align='center' class='animate__animated animate__fadeIn wow'>Собаки</h1>
+				</section>
+				<section class="popug">
 
-		<div class='block1 animate__animated animate__fadeIn wow'>
-			<img src='https://4lapy.ru/resize/800x498/upload/medialibrary/5ef/5ef6110a8811ba93ea64083e8cb9cebf.jpg' alt='Бигль' class='img_1' >
-			<span class='text1'>Бигль</span>
-			<span class='text_1'><b>Цена:</b> 8000р.</span>
-			<input name='input1' class='input1' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block2 animate__animated animate__fadeIn wow'>
-			<img src='https://yandex-images.clstorage.net/u4vp82d18/1dc943unx9p/-PhwXeohEFZfzWecS1PcVPbRZkaRLRRaywNtbGXOGAONNJt-fuHyR1zwdP1meZprj3Esti6_v0bZ8s-wKRkLeS2oo1ststLWH82k3g2RnZY00qRHlCzUzN7Frbl0jM8RnDaP8fF_r49Suu53-ifFO4_mebZpOW4WwuhM5HgJWyD5eLOTpLjX3BvWJNWD0BWZ6B4rQwDtRdEIWGO2s1xM2hDn2fSztSyC0HMpcTJrz_8NIf39rqqplsDr2Oy8CpJv_L98V2QzmdNQH-EeQFmcGS_QY09JpUvRABik-_rYhRddJ9e_uvdkTR67oHk-6cS8jCCz-ylg7xcLo5DpNEVeInN4NVXksxWWX5Mvl8wUnhohk2QJyKbC0Q4fZbK6nExRmm1VPL8-50Qf-ib3NmyP8cLtOjtsoG5ZiS1fqPlG3uoytTmdozAb2htf5dFAENXXJphtDwdjwVkJVaz-cZ_NXdEqm_K1eysLXjXjc_bhAvOCL7m_4WdomgVmnam7xZwss7_yHKr5UlyaHidVg9FWly2XZojI4kbRDFKr-LhRhpKa5ZH8v_YmCxRwq3F5ZQw2y696cyZgqJNG7Vhk-cyf73u_NVHt811W1tfnXEvanhPhUyvKSC0JFILRJTyymYaTFGmaNDL-48rROua2MunF_Q9oc3xlKCheC6NZIPBH3WG29TUV7zIQFB7dJJTD1F3b5FKqD0PpRlUPHyR_utZEk9rgGPU9tK3O3bnpfPumSD4PY_V25Chm1cNmHKy5SZvqODfwWO6z0tSR1aLQQ5cc2KUQakUOpgcVwNpjuHUTgpUUoVv6tnlhTBF7ojC35wt4RWEzuutooRmEa19k9YXTJjL8cJ_tvhZdGp9lXExT1pjoHGuCCCaPnQhZp_P8lsTbk-sYdLX5a4NUPG4_uicCfAmvvT2jai6fyOSQ5DaOkmA49zGYIjjXW9ka4B2LGRnWpZDqDsiiAh1EVW-0Mc' alt='Мопс' class='img_2'>
-			<span class='text2'>Мопс</span>
-			<span class='text_2'><b>Цена:</b> 10000р.</span>
-			<input name='input2' class='input1' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block3 animate__animated animate__fadeIn wow'>
-			<img src='https://kartinkin.net/pics/uploads/posts/2022-07/1658428672_37-kartinkin-net-p-polskaya-ovcharka-zhivotnie-krasivo-foto-41.jpg' alt='Немецкая овчарка' class='img_3'>
-			<span class='text3'>Немецкая овчарка</span>
-			<span class='text_3'><b>Цена:</b> 9500р.</span>
-			<input name='input3' class='input3' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block4 animate__animated animate__fadeIn wow'>
-			<img src='https://pastba.ru/upload/information_system_1/0/2/2/item_22/item_22.jpg' alt='Вельш-корги' class='img_4'>
-			<span class='text4'>Вельш-корги</span>
-			<span class='text_4'><b>Цена:</b> 35000р.</span>
-			<input name='input4' class='input4' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block5 animate__animated animate__fadeIn wow'>
-			<img src='https://faunistics.com/wp-content/uploads/2019/05/6-10.jpg' alt='Такса' class='img_5'>
-			<span class='text5'>Такса</span>
-			<span class='text_5'><b>Цена:</b> 4000р.</span>
-			<input name='input5' class='input5' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block6 animate__animated animate__fadeIn wow'>
-			<img src='https://usobaki.com/wp-content/uploads/Sibirskiy-haski.jpg' alt='Сибирский хаски' class='img_6'>
-			<span class='text6'>Сибирский хаски</span>
-			<span class='text_6'><b>Цена:</b> 10000р.</span>
-			<input name='input6' class='input6' type='submit' value="submit" form='form'>
-		</div>
-
-		<h1 align='center' class='animate__animated animate__fadeIn wow'>Кошки</h1>
-
-		<div class='block7 animate__animated animate__fadeIn wow'>
-			<img src='https://socialcafe.ru/wp-content/uploads/2022/06/1-13.jpg' alt='Мейн-кун' class='img_7'>
-			<span class='text7'>Мейнкун</span>
-			<span class='text_7'><b>Цена:</b> 12000р.</span>
-			<input name='input7' class='input7' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block8 animate__animated animate__fadeIn wow'>
-			<img src='https://zooblog.ru/wp-content/uploads/2021/06/picture-10-5.jpeg' alt='Ориентальная кошка' class='img_8'>
-			<span class='text8'>Ориентальная кошка</span>
-			<span class='text_8'><b>Цена:</b> 12000р.</span>
-			<input name='input8' class='input8' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block9 animate__animated animate__fadeIn wow'>
-			<img src='https://natalyland.ru/wp-content/uploads/b/0/9/b09004b735424b3ef4d41bfd0ed5e31c.jpeg' alt='Абиссинская кошка' class='img_9'>
-			<span class='text9'>Абиссинская кошка</span>
-			<span class='text_9'><b>Цена:</b> 10000р.</span>
-			<input name='input9' class='input9' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block10 animate__animated animate__fadeIn wow'>
-			<img src='https://aroundpet.ru/wp-content/uploads/poroda-manchkin.jpg' alt='Манчкин' class='img_10'>
-			<span class='text10'>Манчкин</span>
-			<span class='text_10'><b>Цена:</b> 50000р.</span>
-			<input name='input10' class='input10' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block11 animate__animated animate__fadeIn wow'>
-			<img src='https://koshek.ru/assets/components/gallery/connector.php?action=web/phpthumb&ctx=web&w=1600&h=800&zc=0&far=&q=90&src=%2Fassets%2Fgallery%2F66%2F973.jpg' alt='Русская голубая кошка' class='img_11'>
-			<span class='text11'>Русская голубая кошка</span>
-			<span class='text_11'><b>Цена:</b> 40000р.</span>
-			<input name='input11' class='input11' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block12 animate__animated animate__fadeIn wow'>
-			<img src='https://chudo-prirody.com/uploads/posts/2021-08/1628230314_40-p-ekzoticheskaya-koshka-foto-41.jpg' alt='Экзотическая кошка' class='img_12'>
-			<span class='text12'>Экзотическая кошка</span>
-			<span class='text_12'><b>Цена:</b> 15000р.</span>
-			<input name='input12' class='input12' type='submit' value="submit" form='form'>
-		</div>
-
-		<h1 align='center' class='animate__animated animate__fadeIn wow'>Попугаи</h1>
-
-		<div class='block13 animate__animated animate__fadeIn wow'>
-			<img src='https://perepela.ru/images/vonistye-popugai-optom.jpg' alt='Волнистые' class='img_13'>
-			<span class='text13'>Волнистые попугаи</span>
-			<span class='text_13'><b>Цена:</b> от 500р.</span>
-			<input name='input13' class='input13' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block14 animate__animated animate__fadeIn wow'>
-			<img src='https://animalreader.ru/wp-content/uploads/2014/11/1280px-Agapornis_roseicollis_and_Agapornis_fischeri_Wroclaw_zoo-2-e1414857040656.jpg' alt='Неразлучники' class='img_14'>
-			<span class='text14'>Неразлучники</span>
-			<span class='text_14'><b>Цена:</b> от 1000р. до 17000р.</span>
-			<input name='input14' class='input14' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block15 animate__animated animate__fadeIn wow'>
-			<img src='https://zookakadu.ru/wp-content/uploads/b/f/f/bff79a78acdcc3f6abeb4a126e2d7703.jpeg' alt='Воробьиные' class='img_15'>
-			<span class='text15'>Воробьиные попугаи</span>
-			<span class='text_15'><b>Цена:</b> до 1500р.</span>
-			<input name='input15' class='input15' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block16 animate__animated animate__fadeIn wow'>
-			<img src='https://zookakadu.ru/wp-content/uploads/d/4/a/d4a298e5e4611eb5a93306057ceee6f1.jpeg' alt='Корелла' class='img_16'>
-			<span class='text16'>Корелла</span>
-			<span class='text_16'><b>Цена:</b> от 5000р.</span>
-			<input name='input16' class='input16' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block17 animate__animated animate__fadeIn wow'>
-			<img src='https://wikipet.ru/wp-content/uploads/2018-02/1519741917_rozella1.jpg' alt='Розелла' class='img_17'>
-			<span class='text17'>Розелла</span>
-			<span class='text_17'><b>Цена:</b> от 8000р.</span>
-			<input name='input17' class='input17' type='submit' value="submit" form='form'>
-		</div>
-
-		<div class='block18 animate__animated animate__fadeIn wow'>
-			<img src='https://animalreader.ru/wp-content/uploads/2014/10/jaco1-e1413385220288.jpg' alt='Жако' class='img_18'>
-			<span class='text18'>Жако</span>
-			<span class='text_18'><b>Цена:</b> от 60000р.</span>
-			<input name='input18' class='input18' type='submit' value="submit" form='form'>
+				</section>
+			</div>
+			<section class="form_sozdat">
+				<button type='submit' class='sozdat'>Создать объявление</button>
+			</section>
 		</div>
 	</div>
 </main>
